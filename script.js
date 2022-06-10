69584e53f558398d8b7c5f93046037e46b7ecbbf
Justin Townley <justin.d.townley@gmail.com>
2022-06-10 02:17:56 -0700

    Test functionality of computerPlay
    Tested 1000 runs of the function repeatedly. Counted
    the number of each possibility to see if random.
    Put a debug point on the default of the switch
    with no calls. Success!

--- script.js
+++ script.js
@@ -3,20 +3,32 @@
 // computer randomly chooses rock paper or scissors
 // result of match is player wins, computer wins, or tie
 // result is returned to console
-
+let countRock = 0;
+let countScissors = 0;
+let countPaper = 0;
 function computerPlay() {
     //finds random number between 0-2
     let chance = Math.floor(Math.random() * 3);
     switch (chance) {
         case 0:
+            countRock++;
             return "rock";
         case 1:
+            countPaper++;
             return "paper";
         case 2:
+            countScissors++;
             return "scissors";    
         default:
             console.log("computer had issues");
             return "error!"
     }
 }
-console.log(computerPlay());
+for (let i = 0; i < 1000; i++) {
+    console.log(computerPlay());
+}
+console.log(
+    `rocks = ${countRock}
+    papers = ${countPaper}
+    scissors = ${countScissors}`
+);
