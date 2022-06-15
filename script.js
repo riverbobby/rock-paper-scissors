fc64cef9df8bc6914ab37db2a10abe4a92bed343
Justin Townley <justin.d.townley@gmail.com>
2022-06-15 02:01:45 -0700

    Complete functionality of game
    Still need to make it pretty

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+//global variables
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
 const playButton = document.querySelector('#btn-play');
 playButton.addEventListener('click', game);
 
-function game() {
-    let wins = 0;
-    let losses = 0;
-    let ties = 0;
-    let gameOver = false;
+function game() {//starts the game
     const bod = document.querySelector('#body');
     const gamePageContainer = document.querySelector('#game-page-container');
 
@@ -14,83 +15,80 @@ function game() {
     bod.removeChild(loadPageContainer);
 
     //add to display #gamePageContainer and components
-    gamePageContainer.style.display = 'flex';
+    gamePageContainer.style.visibility = 'visible';
     
     //activate button start over
     const startOver = document.querySelector('#btn-start-over');
     startOver.onclick = () => window.location.reload();
 
-    //initialize stats
-    const winsDisplay = document.querySelector('#wins');
-    const lossesDisplay = document.querySelector('#losses');
-    const tiesDisplay = document.querySelector('#ties');
-    winsDisplay.textContent = `Wins: ${wins}`;
-    lossesDisplay.textContent = `Losses: ${losses}`;
-    tiesDisplay.textContent = `Ties: ${ties}`;
-    
-
-    //initialize game
-    while (!gameOver) {
-    
-        //add onclick handlers for imgs
-        const rock = document.querySelector('#rock');
-        rock.addEventListener('click', playRound('rock', computerPlay()));
-        const paper = document.querySelector('#paper');
-        const scissors = document.querySelector('#scissors');
-
-        //display stats
-        if (wins > 4 || losses > 4) {
-            gameOver = true;
-        }
-    }
-    //display final-result
-    const result = document.querySelector('#final-result');
-    result.style.display = 'block';
+    refreshStats();
+    refreshSelectors();
 }
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {//plays a round
     //initialize initialResult
     const initialResult = document.querySelector('#initial-result');
 
-    if (playerSelection === computerSelection) {
-        initialResult.textContent = "Tie!";
-        ties++;
-        return;
-    } 
     //variables for messages
     let rockBreaksScissors = 'Rock breaks Scissors';
     let paperCoversRock = 'Paper covers Rock';
     let scissorsCutsPaper = 'Scissors cuts paper';
-    let win = 'you win this round!';
-    let lose = 'you lose this round!';
+    let winRound = 'you win this round!';
+    let loseRound = 'you lose this round!';
     
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            initialResult.textContent = `${paperCoversRock} + ${lose}`;
-            losses++;
+    if (playerSelection === computerSelection) {
+        initialResult.textContent = "Tie!";
+        ties++;
+    } else {
+        if (playerSelection === 'rock') {
+            if (computerSelection === 'paper') {
+                initialResult.textContent = `${paperCoversRock}, ${loseRound}`;
+                losses++;
+            } else {
+                initialResult.textContent = `${rockBreaksScissors}, ${winRound}`;
+                wins++;
+            }
+            
+        } else if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                initialResult.textContent = `${paperCoversRock}, ${winRound}`;
+                wins++;
+            } else {
+                initialResult.textContent = `${scissorsCutsPaper} , ${loseRound}`;
+                losses++;
+            }
+            
+        } else { //playerSelection must be 'scissors'
+            if (computerSelection === 'rock') {
+                initialResult.textContent = `${rockBreaksScissors}, ${loseRound}`;
+                losses++;
+            } else {
+                initialResult.textContent = `${scissorsCutsPaper}, ${winRound}`;
+                wins++;
+            }
+        } 
+    }
+
+    refreshStats();
+
+    if (wins > 4 || losses > 4) {//ends game
+        //remove #selection-title and selections
+        const selectionTitle = document.querySelector('#selection-title');
+        const selections = document.querySelector('#selections');
+        const gamePageContainer = document.querySelector('#game-page-container');
+        gamePageContainer.removeChild(selectionTitle);
+        gamePageContainer.removeChild(selections);
+        // display final-result
+        const result = document.querySelector('#final-result');
+        if (wins > 4) {
+            result.textContent = 'YOU ARE VICTORIOUS, YOU BEAT THE COMPUTER!!'; 
         } else {
-            initialResult.textContent = `${rockBreaksScissors} + ${win}`;
-            wins++;
+            result.textContent = 'GAME OVER, YOU HAVE BEEN VANQUISHED!!';
         }
-        
-    } else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            initialResult.textContent = `${paperCoversRock} + ${win}`;
-            wins++;
-        } else {
-            initialResult.textContent = `${scissorsCutsPaper} + ${lose}`;
-            losses++;
-        }
-        
-    } else { //playerSelection must be 'scissors'
-        if (computerSelection === 'rock') {
-            initialResult.textContent = `${rockBreaksScissors} + ${lose}`;
-            losses++;
-        } else {
-            initialResult.textContent = `${scissorsCutsPaper} + ${win}`;
-            wins++;
-        }
+
+    } else {//keep playing
+        refreshSelectors();
     }
 }
 function computerPlay() {
@@ -107,4 +105,29 @@ function computerPlay() {
             console.log("computer had issues");
             return "error!"
     }
+}
+
+function refreshStats() {
+    const winsDisplay = document.querySelector('#wins');
+    const lossesDisplay = document.querySelector('#losses');
+    const tiesDisplay = document.querySelector('#ties');
+    winsDisplay.textContent = `Wins: ${wins}`;
+    lossesDisplay.textContent = `Losses: ${losses}`;
+    tiesDisplay.textContent = `Ties: ${ties}`;
+}
+
+function refreshSelectors() {
+    let computerSelection = computerPlay();
+    const rock = document.querySelector('#rock');
+    rock.onclick = () => {
+        playRound('rock', computerSelection)
+    };
+    const paper = document.querySelector('#paper');
+    paper.onclick = () => {
+        playRound('paper', computerSelection)
+    };
+    const scissors = document.querySelector('#scissors');
+    scissors.onclick = () => {
+        playRound('scissors', computerSelection)
+    };
 }
